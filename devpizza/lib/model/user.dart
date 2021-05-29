2a0f5b20ead0477abed299335d18a52379a91eca
Thales Loreto <thales.loreto@ifood.com.br>
2021-05-29 16:34:30 -0300

    Add Bloc, events and model

---
+++ devpizza/lib/model/user.dart
@@ -0,0 +1,21 @@
+class User {
+  String name;
+  String street;
+  int streetNumber;
+  String email;
+  String password;
+
+  User({this.name, this.email, this.street, this.streetNumber, this.password});
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{};
+
+    // TODO: Map database column names from field names
+
+    return map;
+  }
+
+  User.fromMap(Map<String, dynamic> map) {
+    // TODO: Map fields from database column names
+  }
+}
